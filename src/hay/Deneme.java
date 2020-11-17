9aa7815af5d416524881a890999c67a537710bce
huseyin34-It <68127607+huseyin34-It@users.noreply.github.com>
2020-11-17 22:58:33 +0300

    Update Deneme.java
    github dn locale

--- src/hay/Deneme.java
+++ src/hay/Deneme.java
@@ -6,5 +6,7 @@ public class Deneme {
 		System.out.println("Deneme 17.11.20 de yazildi - Saat 17:26");
 		System.out.println("Deneme 17.11.20 de yazildi - Saat 18:58 - bunu ikinci olarak yazdik ");
 		System.out.println( " adim huseyin ");
+				System.out.println( " gihub dab locale ");
+
 	}
 }
